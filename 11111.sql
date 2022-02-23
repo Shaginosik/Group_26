ddf3f355e5f77a9f9407421a7f5ecb41ec340323
Shaginosik <93489776+Shaginosik@users.noreply.github.com>
2022-02-23 20:31:28 +0300

    add111

--- 11111.sql
+++ 11111.sql
@@ -1,117 +1,200 @@
+-- 1) Создать таблицу employees
+--- id. serial,  primary key,
+--- employee_name. Varchar(50), not null
+-- 2) Наполнить таблицу employee 70 строками. (выполнено через Python)
 -- 1. Вывести все поля и все строки.
- select * from students 
- 
--- 2. Вывести всех студентов в таблице
- select name as students  from students 
- 
--- 3. Вывести только Id пользователей
- select  id from students 
- 
--- 4. Вывести только имя пользователей
- select name from students 
- 
--- 5. Вывести только email пользователей
- select email from students
- 
--- 6. Вывести имя и email пользователей
-  select name, email from students
-  
--- 7. Вывести id, имя, email и дату создания пользователей
-  select id, name, email, created_on  from students
-  
--- 8. Вывести пользователей где password 12333
- select  * from students
- where password='12333'
- 
--- 9.  Вывести пользователей которые были созданы 2021-03-26 00:00:00
-  select  * from students
-  where created_on='2021-03-26 00:00:00'
- 
--- 10. Вывести пользователей где в имени есть слово Анна
-  select * from  students
-  where name like '%Anna%' 
-  
--- 11. Вывести пользователей где в имени в конце есть 8
-   select * from  students
-   where name like '%8' 
-   
--- 12. Вывести пользователей где в имени в есть буква а
-    select * from  students
-    where name like '%a%'
-    
--- 13. Вывести пользователей которые были созданы 2021-07-12 00:00:00
-    select  * from students
-    where created_on='2021-07-12 00:00:00'
-    
--- 14. Вывести пользователей которые были созданы 2021-07-12 00:00:00 и имеют пароль 1m313
-    select  * from students
-    where created_on='2021-07-12 00:00:00' and password='1m313' 
-    
--- 15. Вывести пользователей которые были созданы 2021-07-12 00:00:00 и у которых в имени есть слово Andrey
-    select  * from students
-    where created_on='2021-07-12 00:00:00' and name like '%Andrey%'
-    
--- 16. Вывести пользователей которые были созданы 2021-07-12 00:00:00 и у которых в имени есть цифра 8
-    select  * from students
-    where created_on='2021-07-12 00:00:00' and name like '%8%'
-    
--- 17. Вывести пользователя у которых id равен 110
-    select  * from students
-    where id = 110
-    
--- 18. Вывести пользователя у которых id равен 153
-    select  * from students
-    where id = 153
-    
--- 19. Вывести пользователя у которых id больше 140
-    select  * from students
-    where id > 140
-    
--- 20. Вывести пользователя у которых id меньше 130
-    select  * from students
-    where id < 130
-    
--- 21. Вывести пользователя у которых id меньше 127 или больше 188
-    select  * from students
-    where id < 127 or id > 188
-    
--- 22. Вывести пользователя у которых id меньше либо равно 137
-    select  * from students
-    where id <= 137
+create table employees(
+id serial primary key,
+employee_name Varchar(50) not null
+)
 
--- 23. Вывести пользователя у которых id больше либо равно 137
-    select  * from students
-    where id >= 137
-    
--- 24. Вывести пользователя у которых id больше 180 но меньше 190
-    select  * from students
-    where id > 180 and id < 190
-    
--- 25. Вывести пользователя у которых id между 180 и 190
-    select  * from students
-    where id  between  180 and  190
-    
--- 26. Вывести пользователей где password равен 12333, 1m313, 123313
-    select  * from students
-    where password in ('12333', '1m313', '123313')
-        
--- 27. Вывести пользователей где created_on равен 2020-10-03 00:00:00, 2021-05-19 00:00:00, 2021-03-26 00:00:00
-    select  * from students
-    where created_on in ('2020-10-03 00:00:00', '2021-05-19 00:00:00', '2021-03-26 00:00:00')
-    
--- 28. Вывести минимальный id 
-    select min(id) from students
-    
--- 29. Вывести максимальный.
-    select max(id) from students
-    
--- 30. Вывести количество пользователей
-    select count(name) from students
-    
--- 31. Вывести id пользователя, имя, дату создания пользователя. Отсортировать по порядку возрастания даты добавления пользоватлеля.
-    select id, name, created_on from students
-    order by created_on 
-    
--- 32. Вывести id пользователя, имя, дату создания пользователя. Отсортировать по порядку убывания даты добавления пользоватлеля.
-    select id, name, created_on from students
-    order by created_on desc 
+-- 1. Вывести все поля и все строки.
+ 
+select * from employees
+
+-- 3) Создать таблицу salary
+--- id. Serial  primary key,
+--- monthly_salary. Int, not null
+-- 4) Наполнить таблицу salary 15 строками: 
+
+create table salary(
+ id serial primary key,
+monthly_salary int not null
+)
+
+select * from salary 
+
+insert into salary(monthly_salary) 
+values (1000),
+       (1100),
+       (1200),
+       (1300),
+       (1400),
+       (1500),
+       (1600),
+       (1700),
+       (1800),
+       (1900),
+       (2000),
+       (2100),
+       (2200),
+       (2300),
+       (2400);
+       
+-- 5) Создать таблицу employee_salary
+--- id. Serial  primary key,
+--- employee_id. Int, not null, unique
+--- salary_id. Int, not null
+--6) Наполнить таблицу employee_salary 40 строками:
+--- в 10 строк из 40 вставить несуществующие employee_id
+      
+ create table employee_salary(
+ id serial primary key,
+employee_id int not null unique,
+salary_id int not null
+)
+
+select * from employee_salary
+
+insert into employee_salary(employee_id, salary_id) 
+values (1, 1),
+       (2, 5),
+       (3, 6),
+       (4, 3),
+       (5, 7),
+       (6, 11),
+       (7, 4),
+       (8, 2),
+       (9, 6),
+       (10, 9),
+       (11, 12),
+       (12, 15),
+       (13, 13),
+       (14, 5),
+       (15, 4),
+       (16, 2),
+       (17, 6),
+       (18, 7),
+       (19, 10),
+       (20, 10),
+       (21, 15),
+       (22, 8),
+       (23, 9),
+       (24, 6),
+       (25, 3),
+       (26, 2),
+       (27, 7),
+       (28, 11),
+       (29, 12),  
+       (30, 11),  
+       (100, 1),
+       (200, 5),
+       (300, 6),
+       (400, 3),
+       (500, 7),
+       (600, 11),
+       (700, 4),
+       (800, 2),
+       (900, 6),
+       (1000, 9);      
+      
+-- 7) Создать таблицу roles
+--- id. Serial  primary key,
+--- role_name. int, not null, unique
+-- 8) Поменять тип столба role_name с int на varchar(30)
+-- 9) Наполнить таблицу roles 20 строками:
+
+
+create table roles(
+id serial primary key,
+role_name int not null unique 
+)
+
+select * from roles
+
+ALTER TABLE roles
+ALTER COLUMN role_name type Varchar(30);
+
+insert into roles(role_name)
+values ('Junior Python developer'),
+	 ('Middle Python developer'),
+	 ('Senior Python developer'),
+	 ('Junior Java developer'),
+	 ('Middle Java developer'),
+	 ('Senior Java developer'),
+	 ('Junior JavaScript developer'),
+	 ('Middle JavaScript developer'),
+	 ('Senior JavaScript developer'),
+	 ('Junior Manual QA engineer'),
+	 ('Middle Manual QA engineer'),
+	 ('Senior Manual QA engineer'),
+	 ('Project Manager'),
+	 ('Designer'),
+	 ('HR'),
+	 ('CEO'),
+	 ('Sales manager'),
+	 ('Junior Automation QA engineer'),
+	 ('Middle Automation QA engineer'),
+	 ('Senior Automation QA engineer');
+	 
+	
+--	10) Создать таблицу roles_employee
+--- id. Serial  primary key,
+--- employee_id. Int, not null, unique (внешний ключ для таблицы employees, поле id)
+--- role_id. Int, not null (внешний ключ для таблицы roles, поле id)
+-- 11) Наполнить таблицу roles_employee 40 строками:
+
+	create table roles_employee(
+	id serial primary key,
+	employee_id int not null unique,
+	role_id int not null,
+	foreign key (employee_id)
+    references employees(id),
+    foreign key (role_id)
+    references roles(id)  
+	)
+	
+	select * from roles_employee
+	
+	insert into roles_employee(employee_id, role_id) 
+values (1, 1),
+       (2, 5),
+       (3, 6),
+       (4, 3),
+       (5, 7),
+       (6, 11),
+       (7, 4),
+       (8, 2),
+       (9, 18),
+       (10, 9),
+       (11, 12),
+       (12, 20),
+       (13, 13),
+       (14, 5),
+       (15, 4),
+       (16, 2),
+       (17, 6),
+       (18, 7),
+       (19, 10),
+       (20, 10),
+       (21, 15),
+       (22, 8),
+       (23, 14),
+       (24, 6),
+       (25, 15),
+       (26, 2),
+       (27, 7),
+       (28, 11),
+       (29, 12),  
+       (30, 18),
+       (31, 10),
+       (32, 15),
+       (33, 8),
+       (34, 16),
+       (35, 20),
+       (36, 3),
+       (37, 17),
+       (38, 7),
+       (39, 11),
+       (40, 19);  
+       
